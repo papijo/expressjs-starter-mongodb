b9c5601e96f303fa0b4b6bf01770856feba698dc
Ebhota Jonathan <joe.ebh100@live.com>
2025-01-01 15:40:42 +0100

    Minor Additions

--- src/enums/config.enum.ts
+++ src/enums/config.enum.ts
@@ -1,12 +1,12 @@
 export enum ServerEnvOptions {
-  DEVELOPMENT = 'development',
-  STAGING = 'staging',
-  TESTING = 'testing',
-  PRODUCTION = 'production',
+  DEVELOPMENT = "development",
+  STAGING = "staging",
+  TESTING = "testing",
+  PRODUCTION = "production",
 }
 
 export enum DebugOptions {
-  DEVELOPMENT = 'dev',
-  STAGING = 'stag',
-  PRODUCTION = 'prod',
+  DEVELOPMENT = "dev",
+  STAGING = "stag",
+  PRODUCTION = "prod",
 }
